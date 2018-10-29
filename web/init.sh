06c7a073b6164d9b437e21d56b5e4b4c57cba320
keyzi <keyzi.ru@gmail.com>
2018-10-29 21:00:38 +0300

    gunicorn5

--- web/init.sh
+++ web/init.sh
@@ -1,7 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-# restart nginx
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-
-# creare symbolic links to gunicorn configs
-sudo -s ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-sudo -s ln -sf /home/box/web/etc/django-gunicorn.conf  /etc/gunicorn.d/django-gunicorn.conf
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
